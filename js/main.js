1693fba1f96db411af24abe62750cf8c88ff3c0f
BenBerni <benbernihit@gmail.com>
2024-06-23 11:57:16 +0300

    e

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var data1 = {
         labels: [0],
         datasets: [{
@@ -56,68 +56,64 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('myChart2'),
         config2
     );
-});
 
-async function GetDataFormSheet() {
-    let apiURL = `https://script.google.com/macros/s/AKfycbxQ-sZtcPznUa6MYrPG2Pk9u_ur7ggkkvoAPdEfgTd5VAkSKQ6Dp4JUwycphzb0Wq5z/exec`;
-    let datas = await fetch(apiURL).then(res => res.json());
-    console.log(datas);
-    let dts = datas.slice(0, 10).reverse(); // Get the last 10 elements and reverse them
-    console.log(dts);
-    dts.forEach(function(dt) {
-        data1.labels.push(dt.time);
-        data1.datasets[0].data.push(dt.temp);
-        data2.labels.push(dt.time);
-        data2.datasets[0].data.push(dt.adc);
-    });
-    myChart1.update();
-    myChart2.update();
-}
-
-async function processData() {
-    await GetDataFormSheet();
-
-    DataRef.child('NhietDo').on('value', (snapshot) => {
-        const temp = snapshot.val();
-        var now = new Date();
-        now = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
-        if (data1.datasets[0].data.length >= 10) {
-            data1.labels.shift();
-            data1.datasets[0].data.shift();
-        }
-        data1.labels.push(now);
-        data1.datasets[0].data.push(temp);
+    async function GetDataFormSheet() {
+        let apiURL = `https://script.google.com/macros/s/AKfycbxQ-sZtcPznUa6MYrPG2Pk9u_ur7ggkkvoAPdEfgTd5VAkSKQ6Dp4JUwycphzb0Wq5z/exec`;
+        let datas = await fetch(apiURL).then(res => res.json());
+        console.log(datas);
+        let dts = datas.slice(0, 10).reverse(); // Get the last 10 elements and reverse them
+        console.log(dts);
+        dts.forEach(function (dt) {
+            data1.labels.push(dt.time);
+            data1.datasets[0].data.push(dt.temp);
+            data2.labels.push(dt.time);
+            data2.datasets[0].data.push(dt.adc);
+        });
         myChart1.update();
-    });
-
-    DataRef.child('SoDien').on('value', (snapshot) => {
-        const adc = snapshot.val();
-        var now = new Date();
-        now = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
-        if (data2.datasets[0].data.length >= 10) {
-            data2.labels.shift();
-            data2.datasets[0].data.shift();
-        }
-        data2.labels.push(now);
-        data2.datasets[0].data.push(adc);
         myChart2.update();
-    });
-}
-
-processData();
-var DataRef = firebase.database().ref(); // Adjust this based on your Firebase configuration
-
-DataRef.child('DoAm').on('value', (snapshot) => {
-    const humi = snapshot.val();
-    let humidityElement = document.getElementById('humidity');
-    if (humidityElement) {
-        humidityElement.innerHTML = `${humi}<span>%</span>`;
-        document.documentElement.style.setProperty('--humidity-value', humi);
     }
+
+    async function processData() {
+        await GetDataFormSheet();
+
+        var DataRef = firebase.database().ref(); // Adjust this based on your Firebase configuration
+
+        DataRef.child('NhietDo').on('value', (snapshot) => {
+            const temp = snapshot.val();
+            var now = new Date();
+            now = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+            if (data1.datasets[0].data.length >= 10) {
+                data1.labels.shift();
+                data1.datasets[0].data.shift();
+            }
+            data1.labels.push(now);
+            data1.datasets[0].data.push(temp);
+            myChart1.update();
+        });
+
+        DataRef.child('SoDien').on('value', (snapshot) => {
+            const adc = snapshot.val();
+            var now = new Date();
+            now = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+            if (data2.datasets[0].data.length >= 10) {
+                data2.labels.shift();
+                data2.datasets[0].data.shift();
+            }
+            data2.labels.push(now);
+            data2.datasets[0].data.push(adc);
+            myChart2.update();
+        });
+
+        DataRef.child('DoAm').on('value', (snapshot) => {
+            const humi = snapshot.val();
+            let humidityElement = document.getElementById('humidity');
+            if (humidityElement) {
+                humidityElement.innerHTML = `${humi}<span>%</span>`;
+                document.documentElement.style.setProperty('--humidity-value', humi);
+            }
+        });
+    }
+
+    processData();
 });
-
-if (database && database['Acount'] && database['Acount'][0]) {
-    console.log(database['Acount'][0].name);
-}
-
 
